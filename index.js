cc8accf7b2a9b1bfb2d90505418c80581a725c88
Thomas Taylor <thomas.taylor@kineo.com>
2017-11-12 11:17:11 +0000

    Add landing page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cli = require('staticsite-cli');
+var fs = require('fs');
 var http = require("http");
 var path = require("path");
 
@@ -6,10 +7,11 @@ var PORT = process.env.PORT || 5678;
 console.log(`Listening on port ${PORT}`);
 
 var server = http.createServer(function onRequest(req, res) {
-  if(req.method !== 'GET' || req.url !== '/') {
-    return res.end();
+  if(req.method !== 'GET' || req.url !== '/github') {
+    return res.end(fs.readFileSync('public/landing.html'));
   }
   console.log('Handling valid request...');
+  res.end();
 });
 
 server.listen(PORT);
