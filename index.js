0115183bbe6e68f5e54cdb3cf6248d45e895e502
Thomas Taylor <thomas.taylor@kineo.com>
2017-11-12 11:44:02 +0000

    Prepare for github data parsing

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ var http = require("http");
 var path = require("path");
 
 var PORT = process.env.PORT || 5678;
-console.log(`Listening on port ${PORT}`);
 
 var server = http.createServer(function onRequest(req, res) {
-  if(req.method !== 'GET' || req.url !== '/github') {
+  if(req.method !== 'POST' || req.url !== '/github') {
     console.log(`Ignoring invalid request...${req.url}`);
     return res.end(fs.readFileSync('public/landing.html'));
   }
-  console.log('Handling valid request...');
-  res.end();
+  var data = "";
+
+  req.on('data', function(reqData) {
+    data += reqData.toString();
+  });
+  req.on('end', function() {
+    data = JSON.parse(data);
+    console.log(JSON.stringify(data, null, 2));
+    return res.end(fs.readFileSync('public/landing.html'));
+  });
 });
 
+console.log(`Listening on port ${PORT}`);
 server.listen(PORT);
